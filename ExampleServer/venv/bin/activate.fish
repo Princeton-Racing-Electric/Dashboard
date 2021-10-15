d980c12a3f9797ec89a93a0bccaefe4ef2b1702a
sbfisher <sebastian.fisher@canalyst.com>
2021-10-15 15:49:41 -0400

    reconfiguring venv

--- ExampleServer/venv/bin/activate.fish
+++ ExampleServer/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sebastianfisher/Desktop/PRE/dashboard/UI_code/venv"
+set -gx VIRTUAL_ENV "/Users/sebastianfisher/Desktop/PRE/dashboard/UI_code/ExampleServer/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
