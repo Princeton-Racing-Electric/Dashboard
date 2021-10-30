24075d3e5607d620532a050fc4a7a7aa2128d038
Princeton Racing Electric <91397506+Princeton-Racing-Electric@users.noreply.github.com>
2021-10-30 16:47:11 -0400

    Add files via upload

--- Temperature.py
+++ Temperature.py
@@ -47,7 +47,7 @@ def read_temp():
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
 
-
-while True:
-    print(read_temp())
-    time.sleep(1)
+if __name__ == "__main__":
+	while True:
+    		print(read_temp())
+    		time.sleep(1)
