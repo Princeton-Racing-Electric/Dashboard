32b0bcce4aaa64d4f62cb5f8f9780878417924b8
Franchesca Doell <fdoell@princeton.edu>
2022-04-02 16:30:57 -0400

    added print statement

--- Server/Server.py
+++ Server/Server.py
@@ -101,6 +101,7 @@ def get_accel() -> float:
 
 # returns the current temperature in (???) from the temp sensor
 def get_temp() -> float:
+    print(readtemp()[1])
     return read_temp()[1]
 
 def get_volt() -> float:
