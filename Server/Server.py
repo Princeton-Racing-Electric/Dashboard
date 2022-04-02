7be171e1423bd0f312be044dda5ead1ee5186cf7
Franchesca Doell <fdoell@princeton.edu>
2022-04-02 17:04:20 -0400

    subprocess comment out

--- Server/Server.py
+++ Server/Server.py
@@ -228,7 +228,7 @@ def restart():
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
-    subprocess.run("shutdown -h 0", shell=True, check=True)
+    # subprocess.run("shutdown -h 0", shell=True, check=True)
     print(output)
 
 ########################################################################
