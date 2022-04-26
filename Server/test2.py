950ca636d63be974d3872f9d1299df64df4db0ae
Franchesca Doell <fdoell@princeton.edu>
2022-04-26 12:56:03 -0400

    test sound working

---
+++ Server/test2.py
@@ -0,0 +1,26 @@
+from threading import Thread, Timer
+from playsound import playsound
+
+#Global variable 
+voltage = 8
+temperature = 90
+
+# function to play sound  -- need to set variables, find wav files, and temperature/battery limits
+def playSoundVolt():
+    if (voltage < 10): 
+        #wavFile = input("Enter a wav filename:")
+        playsound(r'C:\\Users\fndoe\OneDrive\Desktop\PRE\Dashboard Code\Dashboard\Server\voltage_alert.wav')
+        print("playing voltage sound w/ .wav")
+    Timer(5, playSoundVolt).start()
+    print("play sound voltage")
+
+
+def playSoundTemp():
+    if (temperature > 80):
+        playsound(r'C:\\Users\fndoe\OneDrive\Desktop\PRE\Dashboard Code\Dashboard\Server\temperature_alert.wav')
+        print("playing temperature sound w. .wav")
+    Timer(60, playSoundTemp).start()
+    print('play sound temperature')
+
+playSoundVolt()
+playSoundTemp()
