0d744b2912207ba96bfc86a96697d0dc10af44b5
Franchesca Doell <fdoell@princeton.edu>
2022-04-29 17:36:45 -0400

    os system command call

--- Server/Server.py
+++ Server/Server.py
@@ -173,14 +173,16 @@ def increment_var():
 def playSoundVolt():
     if (voltage < 10): 
         #wavFile = input("Enter a wav filename:")
-        playsound(r'/home/pi/Desktop/Dashboard/Server/voltage_alert.wav')
+        #playsound(r'/home/pi/Desktop/Dashboard/Server/voltage_alert.wav')
+        os.system('omxplayer /home/pi/Desktop/Dashboard/Server/voltage_alert.wav')
         print("playing voltage sound w/ .wav")
     Timer(5, playSoundVolt).start()
 
 
 def playSoundTemp():
     if (temperature > 80):
-        playsound(r'/home/pi/Desktop/Dashboard/Server/temperature_alert.wav')
+        #playsound(r'/home/pi/Desktop/Dashboard/Server/temperature_alert.wav')
+        os.system('/home/pi/Desktop/Dashboard/Server/temperature_alert.wav')
         print("playing temperature sound w. .wav")
     Timer(60, playSoundTemp).start()
 
