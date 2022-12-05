24d5a82216c54113914c61b4ad4cbf37a171babd
Franchesca Doell <fdoell@princeton.edu>
2022-12-04 20:48:11 -0500

    usb0

--- serialPythonCode.py
+++ serialPythonCode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import serial
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting > 0:
