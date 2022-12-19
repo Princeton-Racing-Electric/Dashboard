67d77f0fed08bb19e1a9ecd61debd6720d5e2ff4
Franchesca Doell <fdoell@princeton.edu>
2022-12-18 20:32:28 -0500

    serial connection code

--- serialPythonCode.py
+++ serialPythonCode.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import serial
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+            exit()
+# run in termial as "sudo python3 serialPythonCode.py > output.txt" 
+# make sure rx of esp32 is connected to tx0 of rasp pi and vice versa
+# the esp32 and the raspberry pi should be grounded together 
             
