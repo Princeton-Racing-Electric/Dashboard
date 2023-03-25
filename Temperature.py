36421995fa231eb898048a7e3b19819b71c8b9dc
fdoell <fdoell@princeton.edu>
2023-03-25 13:09:36 -0400

    add print statement  in temp

--- Temperature.py
+++ Temperature.py
@@ -24,6 +24,7 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
+print(glob.glob(base_dir + '28*'))
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
