0428753ccdfc25c738a723244852cebd0ba66e49
fdoell <fdoell@princeton.edu>
2023-04-28 17:10:32 -0400

    put back later

--- Server/Server.py
+++ Server/Server.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import HallEffect
 import Voltage
 import webbrowser # open a webbrowser
-from Temperature import read_temp 
+#from Temperature import read_temp 
 
 # Constants
 DELAY_TIME = 1
